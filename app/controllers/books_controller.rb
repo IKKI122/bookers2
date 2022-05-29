b155ea9fee9cdd4b3dbc9d7a0c29a04c4cfd5f71
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-05-29 16:30:25 +0000

    [Add]アクセス制限、フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     if @book.save
+      flash[:notice]="You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books=Book.all
@@ -26,11 +27,15 @@ class BooksController < ApplicationController
 
   def edit
     @book=Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
   
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice]="You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
